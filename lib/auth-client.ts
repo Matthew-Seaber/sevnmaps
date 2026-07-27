1cafb38d877534446c641f4dd1e0fa0912361c2d
Matthew <matthewseaber@outlook.com>
2026-07-27 17:44:41 +0100

    Enabled Better Auth dashboard (first push after Vercel deployment)

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,5 +1,10 @@
 import { createAuthClient } from "better-auth/client";
+import { sentinelClient } from "@better-auth/infra/client";
 
 export const authClient = createAuthClient({
   baseURL: process.env.BETTER_AUTH_URL,
+
+  plugins: [
+    sentinelClient()
+  ]
 });
